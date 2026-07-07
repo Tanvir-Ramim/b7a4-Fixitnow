4a881f43a13eac130453d592de9f7732b44c784b
Tanvir Hossan Ramim <132383235+Tanvir-Ramim@users.noreply.github.com>
2026-07-07 12:22:01 +0600

    user and profile schema added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), "env") });
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 
 export default{
-    port: process.env.PORT
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL,
+    app_url: process.env.APP_URL,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import app from "./app";
 import config from "./config";
+import { prisma } from "./lib/primsa";
 const PORT = config.port;
 async function main() {
   try {
+     await prisma.$connect()
+     console.log("connect to the database successfully")
     app.listen(PORT, () => {
       console.log(`Server is running is on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error Starting the Server", error);
+    await prisma.$disconnect()
     process.exit(1);
   }
 }
