921ce8e0823b5b53c5a52406ce8dd3cff9832dc5
Tanvir Hossan Ramim <132383235+Tanvir-Ramim@users.noreply.github.com>
2026-07-10 23:49:30 +0600

    some error fixed

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,4 +1,6 @@
 import Stripe from "stripe";
 import config from "../config";
 
-export const stripe = new Stripe(config.stripe_secret_key)
+const stripe: Stripe = new Stripe(config.stripe_secret_key);
+
+export { stripe };
